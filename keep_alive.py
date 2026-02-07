2ce858bcb516e135a9335b158926012953a4853c
Rim9810 <114338006+Rim9810@users.noreply.github.com>
2026-02-07 18:12:18 +0700

    Update keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot đang chạy ngon lành!"
+
+def run():
+    # Chạy server ở port 8080 (port chuẩn của Replit)
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
